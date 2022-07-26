e55f789f59cd578b9e28951def65695a939972d1
techchrism <chris@techchrism.me>
2022-07-25 20:58:13 -0700

    basic obs recording

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,17 @@
+import 'dotenv/config'
+import OBSWebSocket from 'obs-websocket-js'
 
-console.log('Hello world!')
+async function asyncTimeout(delay: number) {
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve, delay)
+    })
+}
+
+(async () => {
+    const obs = new OBSWebSocket()
+    await obs.connect(`ws://${process.env['OBS_IP']}:${process.env['OBS_PORT']}`, process.env['OBS_PASSWORD'])
+    await obs.call('StartRecord')
+    await asyncTimeout(10 * 1000)
+    await obs.call('StopRecord')
+})()
+
